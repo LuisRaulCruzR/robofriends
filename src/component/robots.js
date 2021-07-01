c079d04637de748c1d117968d6794fb718465932
LuisRaulCruzR <luis.cruzr9820@gmail.com>
2021-06-30 22:00:36 -0600

    first commit

---
+++ src/component/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Graham',
+	username: 'Bret',
+	email: 'sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Ervin Howell',
+	username: 'Antonette',
+	email: 'shanna@melissa.tv'
+},
+{
+	id: 3,
+	name: 'Clementine Bauch',
+	username: 'Samantha',
+	email: 'nathan@yessenia.net'
+},
+{
+	id: 4,
+	name: 'Patricia Lebsack',
+	username: 'Karianne',
+	email: 'julianne@kory.org'
+},
+{
+	id: 5,
+	name: 'Chelsey Bietrich',
+	username: 'Kanren',
+	email: 'lucia_hettinger@annie.ca'
+},
+{
+	id: 6,
+	name: 'Henry Ford',
+	username: 'Pancho',
+	email: 'trip@advisor.com'
+},
+{
+	id: 7,
+	name: 'Cristiano Ronaldo',
+	username: 'Siuu',
+	email: 'fanny@mayo.biz'
+},
+{
+	id: 8,
+	name: 'Roberto Gomez',
+	username: 'Bolaños',
+	email: 'inmanga@april.biz'
+},
+{
+	id: 9,
+	name: 'Lucas Graham Bell',
+	username: 'Bell',
+	email: 'luci@fer.biz'
+},
+{
+	id: 10,
+	name: 'Phineas Flinn',
+	username: 'Candace',
+	email: 'Pherb@april.biz'
+}
+]
